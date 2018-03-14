4a15845bad3b3036891a7bd6065cddaa7739f59c
zaitsev1393 <zaitsev1393@gmail.com>
2018-03-14 15:27:17 +0200

    sort_names description is added

--- sort_names.rb
+++ sort_names.rb
@@ -1,3 +1,10 @@
+# I have a table (users) with two fields id and name. Write a query that returns the first 5 names that contain 'r'. The order should be: 
+# First, words that start with 'r' in alphabetical order.
+# Second, words that contain 'r' in alphabetical order.
+# So - if my list of names is: Alex, Arik, Rebecca, Rose, Brad, Viki, Norman, Drake, Carrie, Taylor, Frank, Robert, Spencer, and Chris.
+# The order of the results should be: Rebecca, Robert, Rose, Brad, Carrie.
+
+
 names = %w(Alex Arik Rebecca Rose Brad Viki Norman Drake Carrie Taylor Frank Robert Spencer Chris)
 
 def sort_names(names, letter)
